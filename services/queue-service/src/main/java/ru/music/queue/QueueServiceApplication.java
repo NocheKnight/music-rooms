b8f0fdd2ac2cf587f192c2c0163781c4552e86ef
lee alex <leealex.2006@mail.ru>
2026-06-19 15:01:36 +0700

    Small changes in configs

--- services/queue-service/src/main/java/ru/music/queue/QueueServiceApplication.java
+++ services/queue-service/src/main/java/ru/music/queue/QueueServiceApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 public class QueueServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(QueueServiceApplication.class, args);
     }
-
 }
